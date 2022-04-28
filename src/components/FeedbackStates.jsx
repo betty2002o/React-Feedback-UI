bf591c768ad98617daae312664bd704ede4231ee
Betty Chiang <betty2002o@gmail.com>
2022-04-27 20:04:36 -0700

    added backend feature

--- src/components/FeedbackStates.jsx
+++ src/components/FeedbackStates.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
 import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackStates() {
+function FeedbackStats() {
   const { feedback } = useContext(FeedbackContext);
 
-  let average =
-    feedback.reduce((acc, cur) => {
-      return acc + cur.rating;
-    }, 0) / feedback.length;
+  // NOTE: simpler average calculation
+  // Calculate ratings avg
+  const average = Math.round(
+    feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length
+  );
 
-  average = average.toFixed(1);
+  // average = average.toFixed(1).replace(/[.,]0$/, '')
+  // average = Math.round(average)
 
   return (
-    <div className="feedbacl-states">
+    <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
-      <h4> Averge Rating: {isNaN(average) ? 0 : average}</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   );
 }
 
-export default FeedbackStates;
+export default FeedbackStats;
